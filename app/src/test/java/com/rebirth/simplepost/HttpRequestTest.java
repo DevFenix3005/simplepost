1b8b7f06fba03c17fe307fef173b1968d168a00b
Roberto Cazarin <robertodmngzczrn@gmail.com>
2021-03-28 23:00:03 -0600

    Update test to new version

--- app/src/test/java/com/rebirth/simplepost/HttpRequestTest.java
+++ app/src/test/java/com/rebirth/simplepost/HttpRequestTest.java
@@ -3,18 +3,17 @@ package com.rebirth.simplepost;
 import com.rebirth.simplepost.services.GreetingService;
 import com.rebirth.simplepost.services.dtos.Greeting;
 import com.rebirth.simplepost.web.controllers.GreetingController;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(GreetingController.class)
 public class HttpRequestTest {
