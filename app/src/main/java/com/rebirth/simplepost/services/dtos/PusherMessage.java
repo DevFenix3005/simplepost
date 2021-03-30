76ac464e425dbc1b713a65ad32dc1490e11f977b
Roberto Cazarin <robertodmngzczrn@gmail.com>
2021-03-30 13:09:28 -0600

    Change PusherMessage property, Date to LocalDateTime

--- app/src/main/java/com/rebirth/simplepost/services/dtos/PusherMessage.java
+++ app/src/main/java/com/rebirth/simplepost/services/dtos/PusherMessage.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -13,6 +13,6 @@ public class PusherMessage<BODY> {
 
     private BODY body;
     private String mensaje;
-    private final Date now = new Date();
+    private final LocalDateTime now = LocalDateTime.now();
 
 }
